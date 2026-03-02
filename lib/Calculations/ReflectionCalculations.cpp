dcaf43a73033fcd13fc958bfe7568bdf642dabf1
JuKimpfler <julius.kimpfler@markdorf-robotics.de>
2026-03-02 17:41:50 +0100

    Initial Commit

--- lib/Calculations/ReflectionCalculations.cpp
+++ lib/Calculations/ReflectionCalculations.cpp
@@ -4,4 +4,4 @@ Vec2 ReflectionCalc::getReflection(Vec2 first, Vec2 second){
     Vec2 vector = Vec2{0,0};
     vector.y = first.y+(second.y-first.y)/(second.x/first.x + 1);
     return vector;
-}
+} 
